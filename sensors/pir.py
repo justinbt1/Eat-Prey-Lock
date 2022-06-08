500e12132481434129ff0f9be749e409f452b732
robert-drop-table <85031828+robert-drop-table@users.noreply.github.com>
2022-06-08 20:12:12 +0100

    Added picamera video capture

--- sensors/pir.py
+++ sensors/pir.py
@@ -1,8 +1,21 @@
+from picamera import PiCamera
 from gpiozero import MotionSensor
 
-pir = MotionSensor(4)
+camera = PiCamera()
+pir = MotionSensor(17)
 
-while True:
+i = 0
+
+
+def capture_video():
     pir.wait_for_motion()
     print("You moved")
+    camera.start_recording(f'images/vid{i}.h264')
     pir.wait_for_no_motion()
+    camera.stop_recording()
+    print("You stopped")
+
+
+while True:
+    capture_video()
+    i += 1
