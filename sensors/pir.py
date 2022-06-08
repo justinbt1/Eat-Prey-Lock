dba9b589a40aa0680d63cfce6ee24666b1be7e09
robert-drop-table <85031828+robert-drop-table@users.noreply.github.com>
2022-06-08 20:24:47 +0100

    Removed print statements

--- sensors/pir.py
+++ sensors/pir.py
@@ -9,11 +9,9 @@ i = 0
 
 def capture_video():
     pir.wait_for_motion()
-    print("You moved")
     camera.start_recording(f'images/vid{i}.h264')
     pir.wait_for_no_motion()
     camera.stop_recording()
-    print("You stopped")
 
 
 while True:
